06569e0e3398fd2d5b8346d9a8f2b3b4fcc9c32a
CeciliaChen <cecilia@pdotf.com>
2023-08-22 18:38:13 -0400

    Initial Commit

---
+++ BoomerangCards/BoomerangCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoomerangCardsApp.swift
+//  BoomerangCards
+//
+//  Created by Cecilia Chen on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct BoomerangCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
